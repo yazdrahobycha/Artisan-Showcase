146c9a38f7ab37890653e0322916e1a637b2c793
Arsen Yavorskyi <ars.yar24@gmail.com>
2024-08-29 17:12:06 +0300

    another try to fix problem on vercel

--- src/components/AuthorPortraits/AuthorPortraits.js
+++ src/components/AuthorPortraits/AuthorPortraits.js
@@ -6,22 +6,23 @@ import { range } from "@/helpers/range";
 
 function AuthorPortraits() {
   return (
-    <ImageCarousel>
-      {range(5).map((_, i) => {
-        // var dimensions = imageSize(`public/img/carlos/${i}.jpg`);
-        return (
-          <>
-            <Image
-              style={{ objectFit: "cover", height: "auto" }}
-              src={`/img/carlos/${i}.jpg`}
-              alt="piska"
-              fill
-              priority
-            />
-          </>
-        );
-      })}
-    </ImageCarousel>
+    // <ImageCarousel>
+    //   {range(5).map((_, i) => {
+    //     // var dimensions = imageSize(`public/img/carlos/${i}.jpg`);
+    //     return (
+    //       <>
+    //         <Image
+    //           style={{ objectFit: "cover", height: "auto" }}
+    //           src={`/img/carlos/${i}.jpg`}
+    //           alt="piska"
+    //           fill
+    //           sizes="100vw"
+    //           priority
+    //         />
+    //       </>
+    //     );
+    //   })}
+    // </ImageCarousel>
   );
 }
 
